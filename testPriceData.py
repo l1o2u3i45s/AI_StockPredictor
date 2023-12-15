345f2d2d07f9f921b2f83564978068e70dfc4f58
xilian <xilian@pixl.world>
2023-12-15 15:55:48 +0800

    OK

--- testPriceData.py
+++ testPriceData.py
@@ -1,7 +1,10 @@
 import torch  
 from torch.utils.data import DataLoader 
 import Model
-import DataService
+import DataService 
+import matplotlib.pyplot as plt
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
@@ -60,18 +63,24 @@ elif trainType == 2:
     testModel = Model.LSTM_Price(dimension = input_DModel).to(device) 
     testModel.load_state_dict(torch.load('./LSTM_Price.pth'))
    
-    totalScore = 0
-    correctCnt = 0
+    outputs_list = []  # 用於存儲模型的輸出
+    labels_list = []   # 用於存儲標籤 
     with torch.no_grad():
         for inputs, inputMask, labels in test_loader: 
             inputs, inputMask, labels = inputs.to(device), inputMask.to(device), labels.to(device)
 
             outputs = testModel(inputs)
  
-
-            print(outputs[0])
-            print(labels[0])
-
-            totalScore +=1
+            outputs_list.extend(outputs.cpu().numpy())
+            labels_list.extend(labels.cpu().numpy())
  
-    print(f"Correct Rate: {correctCnt/totalScore * 100}")
+ # 現在使用 Matplotlib 繪製折線圖
+plt.figure(figsize=(10, 6))
+plt.plot(outputs_list, label='Model Outputs')
+plt.plot(labels_list, label='Labels')
+plt.title('Comparison between Model Outputs and Labels')
+plt.xlabel('Samples')
+plt.ylabel('Values')
+plt.legend()
+plt.show()
+ 
